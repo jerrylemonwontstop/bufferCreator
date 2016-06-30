485ea429e43d28e59ad8f1dbaf7b0d81e27e3928
Jerome Sullivan <jsullivan@ezesoft.com>
2016-06-30 16:18:45 -0500

    changed method naming/assigning for export compatability

--- bufferCreator/makeBUFF.js
+++ bufferCreator/makeBUFF.js
@@ -1,8 +1,8 @@
-bufferTHIS = function(input)
+function bufferTHIS(input)
 {
  var buffer = new Buffer(input);
  return buffer;
 }
 module.exports = {
-    bufferTHIS: bufferThis
+    bufferThis: bufferThis
 }
